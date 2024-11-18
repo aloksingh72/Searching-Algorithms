9329b46c5dc953ffcfd59d30f2bb9871ce636fe9
Alok Singh <aloksingh36757@gmail.com>
2024-11-18 19:38:59 +0530

    linear search updated

--- LinearSearch.java
+++ LinearSearch.java
@@ -2,9 +2,13 @@ class LinearSearch{
     public static void main(String[] args) {
         int[] a ={1,5,9,8,7,6,3,4};
         //method calling
+        linearSearch(a, 9);
 
     }
 
     //method decalration
-    
+    public static void linearSearch(int[] a,int target){
+
+    }
+
 }
