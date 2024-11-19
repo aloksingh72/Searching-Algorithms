69722ed6e335505f40b205057767302bf8ffcba4
Alok Singh <aloksingh36757@gmail.com>
2024-11-19 19:52:40 +0530

    linear searching

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,42 +1,42 @@
 import java.util.Scanner;
 
-class LinearSearch{
-    public static void main(String[] args) {
+class LinearSearch {
+	public static void main(String[] args) {
 
-			Scanner sc = new Scanner(System.in);
-	
-        int[] a ={1,5,9,8,7,6,3,4};
+		Scanner sc = new Scanner(System.in);
 
-			System.out.println("Enter the target element");
-		
+		int[] a = { 1, 5, 9, 8, 7, 6, 3, 4 };
+
+		System.out.println("Enter the target element");
+
 		int target = sc.nextInt();
-			
-        //method calling
-       int indx=  linearSearch(a, target);
 
-		if(indx>=0){
-				System.out.println("Element found at index "+indx);
+		// method calling
+		int indx = linearSearch(a, target);
 
-			}
-else{
-		System.out.println("element is  not found");
+		if (indx >= 0) {
+			System.out.println("Element found at index " + indx);
+
+		} else {
+			System.out.println("element is  not found");
+		}
+
+		sc.close();
+
 	}
-		
 
-    }
+	// method decalration
 
-    //method decalration
+	public static int linearSearch(int[] a, int target) {
 
-    public static int  linearSearch(int[] a,int target){
-	
-			for(int i =0;i<a.length;i++){
-					if(a[i] == target){
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] == target) {
 
-							return i;
-					}
+				return i;
 			}
+		}
 		return -1;
 
-    }
+	}
 
 }
